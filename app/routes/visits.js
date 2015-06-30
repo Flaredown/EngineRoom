6429ec091eec34077b0a0303f83682128d6a5eca
Colin Gorrie <colin@colingorrie.com>
2015-06-30 10:30:53 -0400

    Make engagement and visits use KPI mixing

--- app/routes/visits.js
+++ app/routes/visits.js
@@ -1,62 +1,9 @@
 import Ember from "ember";
+import KPI from "../mixins/kpi";
 import config from "../config/environment";
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(KPI, {
 
-  keenQuerying: Ember.inject.service(),
-  config: config.KPI.visits,
+  config: config.KPI.symptoms
 
-  constructQuery: function(metric) {
-    metric.queryParams.timeframe = this.get("config").filters.baseTimeframe;
-
-    var endDate = d3.time.format("%Y-%m-%dT%H:%M:%S.%LZ")(new Date());  // format Time.now for keen
-    metric.queryParams.timeframe.end = endDate;
-
-    Ember.assert("must supply timeframe end", Ember.isPresent(metric.queryParams.timeframe.end));
-    return new Keen.Query(metric.queryType, metric.queryParams);
-  },
-
-  model() {
-
-    var keenQueries = this.get("config").metrics.map((metric) => {
-      return this.constructQuery(metric);
-    });
-
-    var zip = function(arrays) {
-      return arrays[0].map(function(_, i) {
-        return arrays.map(function(array) {
-          return array[i];
-        });
-      });
-    };
-
-    var self = this;
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-      self.get("keenQuerying.client").run(keenQueries, function(err, res) {
-        if (err) {
-          reject(err);
-        }
-        else {
-          resolve(res);
-        }
-      });
-    }).then(function(response) {
-      return zip([response, self.get("config").metrics]);
-    });
-  },
-
-  setupController(controller, model) {
-
-    var processedModel = model.map(function(m) {
-      var query = m[0],
-          specs = m[1];
-
-      return {
-        specs: specs,
-        raw: query.result
-      };
-    });
-    
-    controller.set("model", processedModel);
-  }
 });
