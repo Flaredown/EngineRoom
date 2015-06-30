15a9c1ac78e8d44930efcebac8ee76233f2d836b
Colin Gorrie <colin@colingorrie.com>
2015-06-30 13:14:58 -0400

    Do looping in emblem instead of hardcoding KPI data

--- app/controllers/engagement.js
+++ app/controllers/engagement.js
@@ -3,9 +3,13 @@ import Ember from "ember";
 var computed = Ember.computed;
 
 export default Ember.Controller.extend({
-  histoOne: computed("model", function() { return this.get("model")[0]; }),
-  lineOne: computed("model", function() { return this.get("model")[1]; }),
-  barOne: computed("model", function() { return this.get("model")[2]; }),
-  stackedAreaOne: computed("model", function() { return this.get("model")[3]; }),
-  ringOne: computed("model", function() { return this.get("model")[4]; })  
+  charts: computed("model", function() {
+    return this.get("model");
+  })
+
+  // histoOne: computed("model", function() { return this.get("model")[0]; }),
+  // lineOne: computed("model", function() { return this.get("model")[1]; }),
+  // barOne: computed("model", function() { return this.get("model")[2]; }),
+  // stackedAreaOne: computed("model", function() { return this.get("model")[3]; }),
+  // ringOne: computed("model", function() { return this.get("model")[4]; })  
 });
